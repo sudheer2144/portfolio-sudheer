a3ce4df127d15126192efec3730c4641ad04e6c7
sudheer2144 <sudheer.ged@gmail.com>
2023-12-26 18:35:22 +0530

    created allDetails object

---
+++ src/All-Details-Object.js
@@ -0,0 +1,14 @@
+const allDetails = {
+  "personal-details": {
+    name: "",
+    email: "",
+    mobile: "",
+  },
+  "social-links": {
+    github: "",
+    linkedin: "",
+    leetcode: "",
+  },
+};
+
+export default allDetails;
