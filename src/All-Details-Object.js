3d0cd011099c35034950697e3fe3fdbc7c706f42
sudheer2144 <sudheer.ged@gmail.com>
2023-12-29 10:31:27 +0530

    Update All-Details-Object.js

--- src/All-Details-Object.js
+++ src/All-Details-Object.js
@@ -1,14 +1,32 @@
 const allDetails = {
-  "personal-details": {
-    name: "",
-    email: "",
-    mobile: "",
+  personalDetails: {
+    name: "Sudheer Geddadi",
+    email: "geddadisudheer@gmail.com",
+    mobile: "(+91)-9989835953",
+    desig: "Full Stack Developer",
+    //
+    socialLinks: {
+      github: "https://github.com/sudheer2144",
+      linkedin: "https://www.linkedin.com/in/geddadi-sudheer",
+      leetcode: "https://leetcode.com/sudheer2144/",
+    },
   },
-  "social-links": {
-    github: "",
-    linkedin: "",
-    leetcode: "",
+  //////////////////
+  about:
+    "Self-motivated and hardworking person with over 2 years of professional experience specializing in web development, ReactJs, Spring Boot, Agile methodology, and testing. seeking for an opportunity to work in a challenging environment to prove my skills and utilizemy knowledge and intelligence in the growth of the organization.",
+  /////////////////
+  work: {
+    role: "Software Engineer (Full Stack)",
+    company: "MEDIA WAVE TECHNOLOGIES",
+    duration: "[Aug-2021 to Present]",
+    points: {
+      p1: "->  2.4 Years hands on experience as Full Stack Developer",
+      p2: "->  Hands on experience on ReactJs based UI development",
+      p3: "->  Hands on experience on Java based server-side development with Spring Boot",
+      p4: "->  Hands on experience on Server-side Database Management.",
+    },
   },
+  //////////////////
 };
 
 export default allDetails;
